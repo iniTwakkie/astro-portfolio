3e59fb554341193439d6d35a653b42421d5f817c
Danie Nell <dnell.personal@gmail.com>
2026-09-18 20:13:28 +0000

    feat: configure Astro redirect for /status to kuma nodes status page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://danienell.com'
+  site: 'https://danienell.com',
+  redirects: {
+    '/status': 'https://kuma.danienell.com/status/nodes'
+  }
 });
